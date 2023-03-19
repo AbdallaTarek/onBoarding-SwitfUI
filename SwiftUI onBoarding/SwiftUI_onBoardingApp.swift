b73dd136747ef141643b2d86a9e62f8dd889c8a9
AbdallaTarek <abdalla.taarek@gmail.com>
2023-03-19 10:47:27 +0200

    Initial Commit

---
+++ SwiftUI onBoarding/SwiftUI_onBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_onBoardingApp.swift
+//  SwiftUI onBoarding
+//
+//  Created by abdullah tarek on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_onBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
